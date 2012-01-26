97aa1d3f3bc9f79ff999b9f42ad120eb6a66a97e
pol <austin.faustino@gmail.com>
2012-01-26 10:02:17 +0800

    added setter
    Signed-off-by: pol <austin.faustino@gmail.com>

--- src/com/onb/formula/area/Circle.java
+++ src/com/onb/formula/area/Circle.java
@@ -5,4 +5,10 @@ class Circle {
 	//pi
 	private static final double pi = 3.1415169;
 	private static final double r = 10;
+	
+	private double diameter;
+
+	private void setDiameter(double diameter) {
+		this.diameter = diameter;
+	}
 }
